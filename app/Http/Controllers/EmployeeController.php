01ed25fa017e666f088561a331b1e38532b7b883
alohathdz <57053156+alohathdz@users.noreply.github.com>
2022-06-17 23:53:52 +0700

    ืทำ validate emp

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -30,7 +30,7 @@ class EmployeeController extends Controller
         $position = Position::whereNotIn('id', function ($query) {
             $query->select('position_id')->from('employees');
         })->where('status', true)->get();
-        
+
         return view('employee_add', compact('position'));
     }
 
@@ -42,17 +42,44 @@ class EmployeeController extends Controller
      */
     public function store(Request $request)
     {
-        $emp = new Employee();
-        $emp->rank = $request->rank;
-        $emp->firstname = $request->firstname;
-        $emp->lastname = $request->lastname;
-        $emp->id_number = $request->id_number;
-        $emp->soldier_number = $request->soldier_number;
-        $emp->corps = $request->corps;
-        $emp->origin = $request->origin;
-        $emp->birthday = dateeng($request->birthday);
-        $emp->rank_date = dateeng($request->rankdate);
-        $emp->education = $request->education;
+        $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'id_number' => 'required|min:13|max:13|unique:employees',
+            'soldier' => 'min:10|max:10|unique:employees',
+            'corps' => 'required',
+            'origin' => 'required',
+            'education' => 'required'
+        ]);
+
+        $position = Position::select('id')->where('number', $request->position)->first();
+        if (Employee::where('position_id', $position)->first()) {
+            $emp = Employee::create([
+                'rank' => $request->rank,
+                'firstname' => $request->firstname,
+                'lastname' => $request->lastname,
+                'id_number' => $request->id_number,
+                'soldier_number' => $request->soldier_number,
+                'corps' => $request->corps,
+                'origin' => $request->origin,
+                'birthday' => dateeng($request->birthday),
+                'rank_date' => dateeng($request->rankdate),
+                'education' => $request->education,
+                'position_id' => $position
+            ]);
+        }
+
+        // $emp = new Employee();
+        // $emp->rank = $request->rank;
+        // $emp->firstname = $request->firstname;
+        // $emp->lastname = $request->lastname;
+        // $emp->id_number = $request->id_number;
+        // $emp->soldier_number = $request->soldier_number;
+        // $emp->corps = $request->corps;
+        // $emp->origin = $request->origin;
+        // $emp->birthday = dateeng($request->birthday);
+        // $emp->rank_date = dateeng($request->rankdate);
+        // $emp->education = $request->education;
     }
 
     /**

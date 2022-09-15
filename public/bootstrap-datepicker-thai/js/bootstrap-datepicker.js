e2bbfad0b89872f92c596600d03a2f5664da6c93
alohathdz <57053156+alohathdz@users.noreply.github.com>
2022-09-15 20:43:56 +0700

    แก้ bootstrap

--- public/bootstrap-datepicker-thai/js/bootstrap-datepicker.js
+++ public/bootstrap-datepicker-thai/js/bootstrap-datepicker.js
@@ -1112,9 +1112,9 @@
 		},
 		headTemplate: '<thead>'+
 							'<tr>'+
-								'<th class="prev"><i class="fa fa-arrow-left"/></th>'+
+								'<th class="prev"><i class="bi bi-arrow-left"/></th>'+
 								'<th colspan="5" class="datepicker-switch"></th>'+
-								'<th class="next"><i class="fa fa-arrow-right"/></th>'+
+								'<th class="next"><i class="bi bi-arrow-right"/></th>'+
 							'</tr>'+
 						'</thead>',
 		contTemplate: '<tbody><tr><td colspan="7"></td></tr></tbody>',

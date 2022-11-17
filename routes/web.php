dba0f36f777294a8a0bcf9b8ce127584f20f8a05
alohathdz <57053156+alohathdz@users.noreply.github.com>
2022-11-17 10:58:46 +0700

    ปิด register

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resources([
